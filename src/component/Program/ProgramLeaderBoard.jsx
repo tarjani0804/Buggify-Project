1409d72384f7c59524b1b83cf50844bb73a3f444
tushar2481 <tusharmotka.19.cs@iite.indusuni.ac.in>
2023-03-24 08:58:55 +0530

    Leaderboard Data Incoming

--- src/component/Program/ProgramLeaderBoard.jsx
+++ src/component/Program/ProgramLeaderBoard.jsx
@@ -1,40 +1,46 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
-import './ProgramLeaderBoard.css';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./ProgramLeaderBoard.css";
 
 const ProgramLeaderBoard = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const gotoPrograms = () => {
-        navigate('/Program')
+  const gotoPrograms = () => {
+    navigate("/Program");
+  };
+  const gotoLeaderBoard = () => {
+    navigate("/ProgramLeaderBoard");
+  };
+  const [leaderList, setLeaderList] = useState();
+  useEffect(() => {
+    async function fetchProfileStats() {
+      const response = await fetch(`http://127.0.0.1:5173/leaderboard`);
+      const jwt = await response.json();
+      setLeaderList(jwt);
     }
-    const gotoLeaderBoard = () => {
-        navigate('/ProgramLeaderBoard')
-    }
+    fetchProfileStats();
+  }, []);
+  console.log(leaderList);
 
-
-    return (
-        <>
-            <div className='program'>
-                <nav className='program-nav'>
-                    <ul className='program-nav-ul'>
-                        <li className='program-nav-ul-li' onClick={gotoPrograms}>
-                            Programs
-                        </li>
-                        <li className='program-nav-ul-li list-selected' onClick={gotoLeaderBoard}>
-                            Leaderboard
-                        </li>
-
-                    </ul>
-                </nav>
-
-
-
-            </div>
-
-        </>
-    )
-}
+  return (
+    <>
+      <div className="program">
+        <nav className="program-nav">
+          <ul className="program-nav-ul">
+            <li className="program-nav-ul-li" onClick={gotoPrograms}>
+              Programs
+            </li>
+            <li
+              className="program-nav-ul-li list-selected"
+              onClick={gotoLeaderBoard}
+            >
+              Leaderboard
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
+};
 
 export default ProgramLeaderBoard;
-
